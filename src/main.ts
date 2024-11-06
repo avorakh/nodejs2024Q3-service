202bae02e9ad0a0c4c85a3d06288d11d82479daf
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-11-06 10:30:34 +0100

    feat: add server port configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { config } from 'dotenv';
+
+const logger = new Logger('main/bootstrap');
+config();
+
+const WEB_SERVER_PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(WEB_SERVER_PORT);
+  logger.log(`Server is running on port: ${WEB_SERVER_PORT}`);
 }
 bootstrap();

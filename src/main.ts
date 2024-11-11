769c61d6a3419b1fae905c26181026900982faac
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-11-11 21:01:08 +0100

    feat: add openapi endpoint

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv';
 import { ServiceHttpExceptionFilter } from './error/service.exception.filter';
+import { serve, setup } from 'swagger-ui-express';
+import { readFile } from 'fs/promises';
+import { resolve } from 'path';
+import { load } from 'js-yaml';
 
 const logger = new Logger('main/bootstrap');
 config();
@@ -27,6 +31,14 @@ async function bootstrap() {
     credentials: false,
   });
 
+  try {
+    const openApiPath = resolve(__dirname, '../doc/api.yaml');
+    const fileContent = await readFile(openApiPath, 'utf8');
+    const openApiDocument = load(fileContent);
+    app.use('/doc', serve, setup(openApiDocument));
+  } catch (error) {
+    console.error('Error loading OpenAPI file:', error);
+  }
   await app.listen(WEB_SERVER_PORT);
   logger.log(`Server is running on port: ${WEB_SERVER_PORT}`);
 }

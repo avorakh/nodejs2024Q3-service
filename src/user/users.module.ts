adaba1f1236eb50dbd361fd21c7e19192f0d9164
Aliaksandr Vorakh <39852199+avorakh@users.noreply.github.com>
2024-11-14 00:43:58 +0100

    feat: integrate user data with typeorm (#11)
    * refactor: update user module
    
    * refactor: splint  user entity and model
    
    * feat: intergrate user table

--- src/user/users.module.ts
+++ src/user/users.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controller/users.controller';
 import { UsersService } from './svc/users.service';
-import { InMemoryUserRepository } from './repository/inmemory.user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { DataSourceModule } from '../orm/orm.datasource';
+import { UserRepository } from './repository/user.repository';
 
 @Module({
+  imports: [DataSourceModule, TypeOrmModule.forFeature([User])],
+  providers: [UserRepository],
+  exports: [UserRepository],
+})
+export class UserRepositoryModule {}
+
+@Module({
+  imports: [UserRepositoryModule],
   controllers: [UsersController],
-  providers: [
-    UsersService,
-    {
-      provide: 'UserRepository',
-      useClass: InMemoryUserRepository,
-    },
-  ],
-  exports: ['UserRepository'],
+  providers: [UsersService],
 })
 export class UsersModule {}

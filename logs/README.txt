76bae7e45e335ccf1d90d9df5b56912fae655962
Aliaksandr Vorakh <39852199+avorakh@users.noreply.github.com>
2024-12-15 08:07:55 +0100

    Task: REST service: Logging & Error Handling and Authentication and Authorization (#18)
    * feat: add logging & error handling (#17)
    
    * feat: add custom logger service
    
    * feat: add logging interceptor
    
    * feat: add custom exception filter
    
    * feat: add error event handling
    
    * feat: add authentication and authorization (#19)
    
    * refactor: update user entity
    
    * feat: modify user service to hash password
    
    * feat: add signup endpoint
    
    * docs: update openapi doc for signup
    
    * feat: add auth login
    
    * docs: update openapi for login
    
    * feat: add token refresh
    
    * refactor: update refresh token
    
    * docs: update openapi doc for refresh
    
    * feat: add auth middleware
    
    * fix: modify docker-compose run (#20)
    
    * refactor: update user entity
    
    * feat: modify user service to hash password
    
    * feat: add signup endpoint
    
    * docs: update openapi doc for signup
    
    * feat: add auth login
    
    * docs: update openapi for login
    
    * feat: add token refresh
    
    * refactor: update refresh token
    
    * docs: update openapi doc for refresh
    
    * feat: add auth middleware
    
    * fix: modify docker-compose run
    
    * docs: update readme file

---
+++ logs/README.txt
@@ -0,0 +1,1 @@
+This is a file to have the logs folder.
--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv';
-import { ServiceHttpExceptionFilter } from './error/service.exception.filter';
 import { serve, setup } from 'swagger-ui-express';
 import { readFile } from 'fs/promises';
 import { resolve } from 'path';
 import { load } from 'js-yaml';
+import { LoggingService } from './logging.service';
+import { LoggingInterceptor } from './log/logging.interceptor';
+import { GlobalExceptionFilter } from './error/global.exception.filter';
 
 const logger = new Logger('main/bootstrap');
+
 config();
 
 const WEB_SERVER_PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useLogger(new LoggingService());
+
+  process.on('uncaughtException', (error) => {
+    logger.error('Uncaught Exception:', error);
+  });
+
+  process.on('unhandledRejection', (reason, promise) => {
+    logger.error(`Unhandled Rejection at:[${promise}], reason:[${reason}]`);
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -23,7 +37,9 @@ async function bootstrap() {
     }),
   );
 
-  app.useGlobalFilters(new ServiceHttpExceptionFilter());
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
 
   app.enableCors({
     origin: '*',
@@ -37,7 +53,7 @@ async function bootstrap() {
     const openApiDocument = load(fileContent);
     app.use('/doc', serve, setup(openApiDocument));
   } catch (error) {
-    console.error('Error loading OpenAPI file:', error);
+    logger.error('Error loading OpenAPI file:', error);
   }
   await app.listen(WEB_SERVER_PORT);
   logger.log(`Server is running on port: ${WEB_SERVER_PORT}`);
